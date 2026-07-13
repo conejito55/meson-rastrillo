ff0b97f5640be7122a0904c05bad7929df995a37
conejito55 <INFO@KROMATICA.ES>
2026-07-13 16:20:09 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -4,457 +4,457 @@ const dishes = [
     "n": 1,
     "name": "Ensaladilla rusa",
     "cat": "raciones",
-    "img": "imagenes/01.png"
+    "img": "01.png"
   },
   {
     "n": 2,
     "name": "Ensalada 3 mares",
     "cat": "raciones",
-    "img": "imagenes/02.png"
+    "img": "02.png"
   },
   {
     "n": 3,
     "name": "Mejillones al vapor",
     "cat": "raciones",
-    "img": "imagenes/03.png"
+    "img": "03.png"
   },
   {
     "n": 4,
     "name": "Mejillones a la vinagreta",
     "cat": "raciones",
-    "img": "imagenes/04.png"
+    "img": "04.png"
   },
   {
     "n": 5,
     "name": "Pulpo a la Gallega",
     "cat": "raciones",
-    "img": "imagenes/05.png"
+    "img": "05.png"
   },
   {
     "n": 6,
     "name": "Boquerones en vinagre",
     "cat": "raciones",
-    "img": "imagenes/06.png"
+    "img": "06.png"
   },
   {
     "n": 7,
     "name": "Salpicón de pulpo y gambón",
     "cat": "raciones",
-    "img": "imagenes/07.png"
+    "img": "07.png"
   },
   {
     "n": 8,
     "name": "Sepia sanglochada",
     "cat": "raciones",
-    "img": "imagenes/08.png"
+    "img": "08.png"
   },
   {
     "n": 9,
     "name": "Sepia con tomate",
     "cat": "raciones",
-    "img": "imagenes/09.png"
+    "img": "09.png"
   },
   {
     "n": 10,
     "name": "Sepia a la plancha",
     "cat": "raciones",
-    "img": "imagenes/10.png"
+    "img": "10.png"
   },
   {
     "n": 11,
     "name": "Sepia frita",
     "cat": "raciones",
-    "img": "imagenes/11.png"
+    "img": "11.png"
   },
   {
     "n": 12,
     "name": "Calamares a la romana",
     "cat": "raciones",
-    "img": "imagenes/12.png"
+    "img": "12.png"
   },
   {
     "n": 13,
     "name": "Chopitos",
     "cat": "raciones",
-    "img": "imagenes/13.png"
+    "img": "13.png"
   },
   {
     "n": 14,
     "name": "Chipirones en su salsa",
     "cat": "raciones",
-    "img": "imagenes/14.png"
+    "img": "14.png"
   },
   {
     "n": 15,
     "name": "Púlpitos con tomate",
     "cat": "raciones",
-    "img": "imagenes/15.png"
+    "img": "15.png"
   },
   {
     "n": 16,
     "name": "Pulpo con alcachofas",
     "cat": "raciones",
-    "img": "imagenes/16.png"
+    "img": "16.png"
   },
   {
     "n": 17,
     "name": "Gambones al ajillo",
     "cat": "raciones",
-    "img": "imagenes/17.png"
+    "img": "17.png"
   },
   {
     "n": 18,
     "name": "Gambas al ajillo",
     "cat": "raciones",
-    "img": "imagenes/18.png"
+    "img": "18.png"
   },
   {
     "n": 19,
     "name": "Gambones en salsa con almejas",
     "cat": "raciones",
-    "img": "imagenes/19.png"
+    "img": "19.png"
   },
   {
     "n": 20,
     "name": "Gambones salteados",
     "cat": "raciones",
-    "img": "imagenes/20.png"
+    "img": "20.png"
   },
   {
     "n": 21,
     "name": "Almejas a la gallega",
     "cat": "raciones",
-    "img": "imagenes/21.png"
+    "img": "21.png"
   },
   {
     "n": 22,
     "name": "Croquetas caseras",
     "cat": "raciones",
-    "img": "imagenes/22.png"
+    "img": "22.png"
   },
   {
     "n": 23,
     "name": "Patatas bravas",
     "cat": "raciones",
-    "img": "imagenes/23.png"
+    "img": "23.png"
   },
   {
     "n": 24,
     "name": "Patatas alioli",
     "cat": "raciones",
-    "img": "imagenes/24.png"
+    "img": "24.png"
   },
   {
     "n": 25,
     "name": "Muslos de pollo campero",
     "cat": "raciones",
-    "img": "imagenes/25.png"
+    "img": "25.png"
   },
   {
     "n": 26,
     "name": "Ternera gallega a la miel",
     "cat": "raciones",
-    "img": "imagenes/26.png"
+    "img": "26.png"
   },
   {
     "n": 27,
     "name": "Magra con tomate",
     "cat": "raciones",
-    "img": "imagenes/27.png"
+    "img": "27.png"
   },
   {
     "n": 28,
     "name": "Albóndigas caseras",
     "cat": "raciones",
-    "img": "imagenes/28.png"
+    "img": "28.png"
   },
   {
     "n": 29,
     "name": "Carrillada",
     "cat": "raciones",
-    "img": "imagenes/29.png"
+    "img": "29.png"
   },
   {
     "n": 30,
     "name": "Berenjena rellena de carne",
     "cat": "raciones",
-    "img": "imagenes/30.png"
+    "img": "30.png"
   },
   {
     "n": 31,
     "name": "Pimiento relleno de carne y arroz",
     "cat": "raciones",
-    "img": "imagenes/31.png"
+    "img": "31.png"
   },
   {
     "n": 32,
     "name": "Merluza fresca a la vasca",
     "cat": "raciones",
-    "img": "imagenes/32.png"
+    "img": "32.png"
   },
   {
     "n": 33,
     "name": "Bacalao a la riojana",
     "cat": "raciones",
-    "img": "imagenes/33.png"
+    "img": "33.png"
   },
   {
     "n": 34,
     "name": "Caracoles",
     "cat": "raciones",
-    "img": "imagenes/34.png"
+    "img": "34.png"
   },
   {
     "n": 35,
     "name": "Codillo asado",
     "cat": "raciones",
-    "img": "imagenes/35.png"
+    "img": "35.png"
   },
   {
     "n": 36,
     "name": "Conejo al ajillo",
     "cat": "raciones",
-    "img": "imagenes/36.png"
+    "img": "36.png"
   },
   {
     "n": 37,
     "name": "Conejo con tomate",
     "cat": "raciones",
-    "img": "imagenes/37.png"
+    "img": "37.png"
   },
   {
     "n": 38,
     "name": "Pulpo sanglochado",
     "cat": "raciones",
-    "img": "imagenes/38.png"
+    "img": "38.png"
   },
   {
     "n": 39,
     "name": "Rabo de toro",
     "cat": "raciones",
-    "img": "imagenes/39.png"
+    "img": "39.png"
   },
   {
     "n": 40,
     "name": "Sopa de mariscos",
     "cat": "sopas",
-    "img": "imagenes/40.png"
+    "img": "40.png"
   },
   {
     "n": 41,
     "name": "Sopa de la casa",
     "cat": "sopas",
-    "img": "imagenes/41.png"
+    "img": "41.png"
   },
   {
     "n": 42,
     "name": "Dorada",
     "cat": "pescados",
-    "img": "imagenes/42.png"
+    "img": "42.png"
   },
   {
     "n": 43,
     "name": "Lubina",
     "cat": "pescados",
-    "img": "imagenes/43.png"
+    "img": "43.png"
   },
   {
     "n": 44,
     "name": "Salmón a la brasa",
     "cat": "pescados",
-    "img": "imagenes/44.png"
+    "img": "44.png"
   },
   {
     "n": 45,
     "name": "Merluza a la plancha",
     "cat": "pescados",
-    "img": "imagenes/45.png"
+    "img": "45.png"
   },
   {
     "n": 46,
     "name": "Merluza fresca a la marinera",
     "cat": "pescados",
-    "img": "imagenes/46.png"
+    "img": "46.png"
   },
   {
     "n": 47,
     "name": "Atún a la brasa",
     "cat": "pescados",
-    "img": "imagenes/47.png"
+    "img": "47.png"
   },
   {
     "n": 48,
     "name": "Emperador a la brasa",
     "cat": "pescados",
-    "img": "imagenes/48.png"
+    "img": "48.png"
   },
   {
     "n": 49,
     "name": "Zarzuela de marisco para dos",
     "cat": "pescados",
-    "img": "imagenes/49.png"
+    "img": "49.png"
   },
   {
     "n": 50,
     "name": "Mariscada",
     "cat": "pescados",
-    "img": "imagenes/50.png"
+    "img": "50.png"
   },
   {
     "n": 51,
     "name": "Gamba roja",
     "cat": "pescados",
-    "img": "imagenes/51.png"
+    "img": "51.png"
   },
   {
     "n": 52,
     "name": "Zamburiñas",
     "cat": "pescados",
-    "img": "imagenes/52.png"
+    "img": "52.png"
   },
   {
     "n": 53,
     "name": "Fritura de pescado",
     "cat": "pescados",
-    "img": "imagenes/53.png"
+    "img": "53.png"
   },
   {
     "n": 54,
     "name": "Pulpo al horno",
     "cat": "pescados",
-    "img": "imagenes/54.png"
+    "img": "54.png"
   },
   {
     "n": 55,
     "name": "Pulpo a la gallega",
     "cat": "pescados",
-    "img": "imagenes/55.png"
+    "img": "55.png"
   },
   {
     "n": 56,
     "name": "Pulpo a la brasa",
     "cat": "pescados",
-    "img": "imagenes/56.png"
+    "img": "56.png"
   },
   {
     "n": 57,
     "name": "Pechuga de pollo a la plancha",
     "cat": "combinados",
-    "img": "imagenes/57.png"
+    "img": "57.png"
   },
   {
     "n": 58,
     "name": "Lomo de cerdo / castaño a la plancha",
     "cat": "combinados",
-    "img": "imagenes/58.png"
+    "img": "58.png"
   },
   {
     "n": 59,
     "name": "Filete de ternera gallega o Angus",
     "cat": "combinados",
-    "img": "imagenes/59.png"
+    "img": "59.png"
   },
   {
     "n": 60,
     "name": "Chuletón gallego o Angus",
     "cat": "carnes",
-    "img": "imagenes/60.png"
+    "img": "60.png"
   },
   {
     "n": 61,
     "name": "Entrecot de vaca rubia gallega, mestiza o añojo",
     "cat": "carnes",
-    "img": "imagenes/61.png"
+    "img": "61.png"
   },
   {
     "n": 62,
     "name": "Solomillo de vaca rubia gallega, mestiza, añojo o ternera rosada",
     "cat": "carnes",
-    "img": "imagenes/62.png"
+    "img": "62.png"
   },
   {
     "n": 63,
     "name": "Parrillada de carnes",
     "cat": "carnes",
-    "img": "imagenes/63.png"
+    "img": "63.png"
   },
   {
     "n": 64,
     "name": "Paella mixta",
     "cat": "arroces",
-    "img": "imagenes/64.png"
+    "img": "64.png"
   },
   {
     "n": 65,
     "name": "Paella de mariscos",
     "cat": "arroces",
-    "img": "imagenes/65.png"
+    "img": "65.png"
   },
   {
     "n": 66,
     "name": "Arroz con carabineros",
     "cat": "arroces",
-    "img": "imagenes/66.png"
+    "img": "66.png"
   },
   {
     "n": 67,
     "name": "Arroz con bogavante",
     "cat": "arroces",
-    "img": "imagenes/67.png"
+    "img": "67.png"
   },
   {
     "n": 68,
     "name": "Espaguetis a la boloñesa",
     "cat": "pastas",
-    "img": "imagenes/68.png"
+    "img": "68.png"
   },
   {
     "n": 69,
     "name": "Tagliatella a la marinera",
     "cat": "pastas",
-    "img": "imagenes/69.png"
+    "img": "69.png"
   },
   {
     "n": 70,
     "name": "Porción de tarta",
     "cat": "postres",
-    "img": "imagenes/70.png"
+    "img": "70.png"
   },
   {
     "n": 71,
     "name": "Pan de Calatrava",
     "cat": "postres",
-    "img": "imagenes/71.png"
+    "img": "71.png"
   },
   {
     "n": 72,
     "name": "Arroz con leche",
     "cat": "postres",
-    "img": "imagenes/72.png"
+    "img": "72.png"
   },
   {
     "n": 73,
     "name": "Fresas con nata",
     "cat": "postres",
-    "img": "imagenes/73.png"
+    "img": "73.png"
   },
   {
     "n": 74,
     "name": "Fruta de campaña",
     "cat": "postres",
-    "img": "imagenes/74.png"
+    "img": "74.png"
   },
   {
     "n": 75,
     "name": "Tarrina de helado",
     "cat": "postres",
-    "img": "imagenes/75.png"
+    "img": "75.png"
   },
   {
     "n": 76,
     "name": "Postres artesanales",
     "cat": "postres",
-    "img": "imagenes/76.png"
+    "img": "76.png"
   }
 ];
 const categoryNames = {"raciones": "Raciones", "sopas": "Sopas", "pescados": "Pescados y mariscos", "combinados": "Platos combinados", "carnes": "Carnes premium", "arroces": "Arroces al momento", "pastas": "Pastas", "postres": "Postres"};
